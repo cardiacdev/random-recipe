c26e62de019bc836fba5aaa7b2a568b67b5f2da2
Mark Loose <m.loose@boewa.de>
2024-02-05 20:14:29 +0100

    Add helper method for query key generation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { SearchParams } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function URLSearchParamsToObj(searchParams: SearchParams) {
+  if (!searchParams) return {};
+
+  if (typeof searchParams === "string") {
+    return Object.fromEntries(new URLSearchParams(searchParams).entries());
+  }
+
+  if (Array.isArray(searchParams)) {
+    return Object.fromEntries(searchParams);
+  }
+
+  if (searchParams instanceof URLSearchParams) {
+    return Object.fromEntries(searchParams.entries());
+  }
+
+  return searchParams;
 }
