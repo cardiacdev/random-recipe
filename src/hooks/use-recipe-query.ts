77e31b4aa124e2e9eb47546fc54fc5e366af8d7f
Mark Loose <m.loose@boewa.de>
2024-02-05 20:27:13 +0100

    Add missing question mark to query string

--- src/hooks/use-recipe-query.ts
+++ src/hooks/use-recipe-query.ts
@@ -7,7 +7,7 @@ const fetchAllRecipes = async (params: SearchParams) => {
   const searchParams = new URLSearchParams(params);
 
   const response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/filter.php${searchParams.toString()}`,
+    `https://www.themealdb.com/api/json/v1/1/filter.php?${searchParams.toString()}`,
   );
   if (!response.ok) {
     const error = await response.json();
