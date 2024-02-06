e5f0a9cf8ec0754e76cf4bc95915c405e6e30e43
Mark Loose <m.loose@boewa.de>
2024-02-06 08:31:50 +0100

    Create button for getting random recipe

---
+++ src/components/random-button.tsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+
+import { Shuffle } from "lucide-react";
+
+import { FilterContext } from "./filter-context";
+import { Button } from "./ui/button";
+
+export const RandomButton = () => {
+  const { regenerateRandomNumber } = useContext(FilterContext);
+  return (
+    <Button className="my-8" size={"lg"} onClick={regenerateRandomNumber}>
+      I'm feeling lucky! <Shuffle />
+    </Button>
+  );
+};
