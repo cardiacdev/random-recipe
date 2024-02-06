3e69f328392b92e2e4d264e1977ce7948a61169e
Mark Loose <m.loose@boewa.de>
2024-02-06 08:31:18 +0100

    Run prettier

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,9 +5,8 @@ export const Header = () => {
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <p>
-
-        <span className="text-2xl font-bold">Random</span>
-        <span className="text-2xl font-bold text-primary">Recipe</span>
+          <span className="text-2xl font-bold">Random</span>
+          <span className="text-2xl font-bold text-primary">Recipe</span>
         </p>
         <div className="flex flex-1 items-center justify-end space-x-4 ">
           <nav className="flex items-center space-x-1">
